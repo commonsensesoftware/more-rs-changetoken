8bfb72ffc8e501573eb017ac8559f5d4256acc72
Chris Martinez <sydefekt@hotmail.com>
2023-11-10 19:56:12 -0800

    Integrate into lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 #![doc = include_str!("../README.md")]
 
-mod token;
+mod composite;
+mod default;
+mod global;
 mod never;
 mod shared;
 mod single;
+mod token;
 
-pub use token::*;
+pub use composite::*;
+pub use default::*;
+pub use global::*;
 pub use never::*;
 pub use shared::*;
 pub use single::*;
+pub use token::*;
